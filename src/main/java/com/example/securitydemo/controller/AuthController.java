c976d5c8202156a6a1c649b3da447c2ff8da9fe4
5chang2 <och8808@gmail.com>
2025-11-28 14:02:11 +0900

    Service 레이어 구성

--- src/main/java/com/example/securitydemo/controller/AuthController.java
+++ src/main/java/com/example/securitydemo/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.example.securitydemo.controller;
 
 
+import com.example.securitydemo.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
@@ -8,7 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
+    private final UserService userService;
 
     @GetMapping("login")
     public String login() {
